4d32d1099b9b7fcd349851d428f57c02d5357095
Evan Borgstrom <evan@fatbox.ca>
2012-11-17 15:55:04 -0500

    More jeditable improvements

--- templates/nextgen_clean/js/fatbox.js
+++ templates/nextgen_clean/js/fatbox.js
@@ -2,26 +2,40 @@
 
     $(document).ready(function() {
         $('.jeditable').each(function(index) {
-            var save_url = $(this).attr('save');
+            var self = this;
+            var timeout = null;
+
+            var save_url = $(self).attr('save');
             if (!save_url) {
                 alert('jeditable element doesn\'t have a "save" attr!');
                 return;
             }
 
+            var add_pencil = function() {
+                timeout = setTimeout(function() {
+                    $(self).append('<i class="icon-pencil"></i>');
+                }, 200);
+            };
+            var del_pencil = function() {
+                $(self).children("i").remove();
+            };
+
             var edit_options = {
                 tooltip: 'Click to edit...',
                 cancel: 'Cancel',
                 submit: 'Save',
+                onedit: del_pencil,
+                onreset: add_pencil,
             };
 
-            if ($(this).attr('indicator')) {
+            if ($(self).attr('indicator')) {
                 edit_options['indicator'] = '/templates/nextgen_fatbox/img/loading-radar.gif';
             } else {
                 edit_options['indicator'] = 'Saving...';
             }
 
-            $(this).editable(save_url, edit_options);
-            $(this).append('<i class="icon-pencil"></i>');
+            $(self).editable(save_url, edit_options);
+            add_pencil();
         });
     });
 
